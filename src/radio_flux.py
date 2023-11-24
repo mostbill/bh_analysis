26413391ead565e85e5aac1223e75ad55c137bad
mostbill <patrickxie104@gmail.com>
2023-11-24 11:01:15 +0100

    updates

--- src/radio_flux.py
+++ src/radio_flux.py
@@ -11,7 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-def plot_and_save_spectrum(frequency, flux, predict, output_filename='radio_spectrum.png'):
+def plot_and_save_spectrum(frequency, flux, survey, predict, output_filename='radio_spectrum.png'):
     """
     Plot a radio spectrum from given frequency and flux data.
     Fit a curve to the data and save the figure.
@@ -29,8 +29,8 @@ def plot_and_save_spectrum(frequency, flux, predict, output_filename='radio_spec
     plt.scatter(predict[0], predict[1], label='predicted at {} GHz'.format(predict[0]))
     
     plt.annotate(f'({predict[0]:.2f}, {predict[1]:.2f})', (predict[0], predict[1]), textcoords="offset points", xytext=(0,10), ha='center')
-    for i, (x, y) in enumerate(zip(frequency, flux)):
-            plt.annotate(f'({x:.2f}, {y:.2f})', (x, y), textcoords="offset points", xytext=(0,10), ha='center')
+    for i, (x, y, survey) in enumerate(zip(frequency, flux, survey)):
+            plt.annotate(f'({x:.2f}, {y:.2f}, {survey:s})', (x, y), textcoords="offset points", xytext=(0,10), ha='center')
 
     # Fit a curve
     def fitted_curve(x, a, b):
@@ -45,14 +45,15 @@ def plot_and_save_spectrum(frequency, flux, predict, output_filename='radio_spec
     plt.plot(x_fit, y_fit, label='{:.2f}*x**{:.2f}'.format(b,a), color='red')
 
     # Set plot parameters and save the figure
-    plt.xlabel('Restframe Frequency')
-    plt.ylabel('Flux')
+    plt.xlabel('Restframe Frequency [GHz]')
+    plt.ylabel('Flux Density [muJy]')
     plt.legend()
     plt.title('Radio Spectrum')
+    print(output_filename)
     plt.savefig(output_filename)
     #plt.show()
 
-def calc_pred_flux(frequency, flux, f_pred, slope=None):
+def calc_pred_flux(frequency, flux, survey, f_pred, slope=None):
     """
     Fit a power-law relation y = b * x^a to frequency and flux data and
     predict the flux at x = 10.
@@ -89,7 +90,7 @@ def calc_pred_flux(frequency, flux, f_pred, slope=None):
     x_predict = f_pred
     y_predict = b * x_predict**a
     
-    plot_and_save_spectrum(frequency, flux, [x_predict, y_predict], output_filename='radio_spectrum.png')
+    plot_and_save_spectrum(frequency, flux, survey, [x_predict, y_predict], output_filename='radio_spectrum.png')
 
     return y_predict
 
@@ -109,6 +110,7 @@ def main():
     
     frequency_data = []
     flux_data = []
+    surveys=[]
 
     while True:
         user_input = input("[INFO]Enter space-separated values in float for freqs (GHz, in restframe)] and fluxes (in muJy) (after each datapoint inputted press enter, input 'y' to stop):\n")
@@ -122,16 +124,17 @@ def main():
             # Assuming the input has at least two elements (for both arrays)
             frequency_data.append(float(elements[0]))
             flux_data.append(float(elements[1]))
+            surveys.append(str(elements[2]))
         except (ValueError, IndexError):
             print("Invalid input. Please enter at least two space-separated values in float.")
 
     # Display the assigned values
-    print("[INFO]Values entered:\nRedshift:{}\nPredicting flux at:{} GHz\nDatapoints at restframe:\nFreqs:{} GHz\nFlux:{} muJy\nSlope (if only one datapoint):{}".format(redshift, f_pred, frequency_data, flux_data, slope_value))
+    print("[INFO]Values entered:\nRedshift:{}\nPredicting flux at:{} GHz\nDatapoints at restframe:\nFreqs:{} GHz\nFlux:{} muJy\nSurveys:{}\nSlope (if only one datapoint):{}".format(redshift, f_pred, frequency_data, flux_data, surveys, slope_value))
 
     # Example usage:
     #slope_value = -0.98  # Set to None if you want to calculate the slope from data
 
-    predicted_flux = calc_pred_flux(np.array(frequency_data)*(1+redshift), np.array(flux_data), f_pred, slope=slope_value)
+    predicted_flux = calc_pred_flux(np.array(frequency_data)*(1+redshift), np.array(flux_data), np.array(surveys), f_pred, slope=slope_value)
     print(f"Predicted flux at 5 GHz (restframe, Jy): {predicted_flux}")
 
 if __name__ == '__main__':
